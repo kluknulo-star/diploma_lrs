5b78b35c649c2fbe2c3271b2c1652e57092e6ec0
kirill-kilyakov <kluknulo@mail.ru>
2023-02-12 16:16:43 +0300

    some fixes

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,6 @@
 
 use App\Statements\Http\Controllers\StatementsController;
 use Illuminate\Support\Facades\Route;
+//вернуть потом ->middleware('auth')
 
-Route::get('/', [StatementsController::class, 'index'])->middleware('auth');
+Route::get('/', [StatementsController::class, 'index']);
