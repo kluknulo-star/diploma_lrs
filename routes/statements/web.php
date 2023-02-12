eecc6c9a7dc254500797364d039ee3ec2140a970
kirill-kilyakov <kluknulo@mail.ru>
2023-02-12 16:14:25 +0300

    create project

--- routes/statements/web.php
+++ routes/statements/web.php
@@ -2,8 +2,8 @@
 
 use App\Statements\Http\Controllers\StatementsController;
 use Illuminate\Support\Facades\Route;
-
-Route::prefix('statements')->middleware('auth')->group(function () {
+//вернуть потом ->middleware('auth')
+Route::prefix('statements')->group(function () {
     Route::get('/', [StatementsController::class, 'index'])->name('statements');
     Route::get('/{statement}', [StatementsController::class, 'show'])->where('id', '[0-9]+')->name('statements.show');
     Route::post('/', [StatementsController::class, 'redirectToFilteredAndSortedPage'])->name('statements');
